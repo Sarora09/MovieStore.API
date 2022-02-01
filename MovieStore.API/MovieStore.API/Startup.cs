ac907c168f1bea6cee9a8c18a6c3202ad7c85996
Sarora09 <52610194+Sarora09@users.noreply.github.com>
2022-01-31 18:32:10 -0700

    Added controller, repository, and interface for movies

--- MovieStore.API/MovieStore.API/Startup.cs
+++ MovieStore.API/MovieStore.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MovieStore.API.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,8 +26,8 @@ namespace MovieStore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddTransient<IMovieRepository, MovieRepository>(); // Dependency Injection
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
